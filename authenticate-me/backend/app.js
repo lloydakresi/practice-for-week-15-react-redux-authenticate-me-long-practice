76419e9982f9c06c64af4e3660453e8073f49bbc
lloyd Akresi <lloydakresi03@gmail.com>
2023-12-05 01:54:29 +0000

    phase 02 done

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
 const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
+const { ValidationError } = require('sequelize');
 
 app.use(morgan('dev'));
 app.use(cookieParser());
@@ -35,5 +37,35 @@ app.use(
 );
 
 app.use(routes);
+
+app.use((req, res, next)=>{
+  const err = new Error("The requested resource could not be found");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource could not be found"];
+  err.status = 404;
+  next(err)
+});
+
+app.use((err, req, res, next)=>{
+  if (err instanceof ValidationError){
+    err.errors = err.errors.map(e => e.message);
+    err.title = 'Validation Error'
+  }
+  next(err);
+});
+
+app.use((err, req, res, next)=>{
+  res.status(err.status||500);
+  console.log(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  })
+
+})
+
+
 
 module.exports = app;
